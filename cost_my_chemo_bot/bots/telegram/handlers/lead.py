6bd5faadfc07c69b0c9fef022f4ae19e8a30427f
f.eremeev <f.eremeev@haut.ai>
2023-01-19 16:12:53 +0400

    fix lead save

--- cost_my_chemo_bot/bots/telegram/handlers/lead.py
+++ cost_my_chemo_bot/bots/telegram/handlers/lead.py
@@ -11,20 +11,24 @@ logger = getLogger(__name__)
 
 
 async def save_lead(message: types.Message):
+    params = {
+        "FIELDS[TITLE]": "Новый лид",
+        "FIELDS[NAME]": message.contact.first_name,
+        "FIELDS[LAST_NAME": message.contact.last_name,
+        # Telegram contact doesn't have an email
+        # "FIELDS[EMAIL][0][VALUE]": message.contact.email,
+        # "FIELDS[EMAIL][0][VALUE_TYPE]": "WORK",
+        "FIELDS[PHONE][0][VALUE]": message.contact.phone_number,
+        "FIELDS[PHONE][0][VALUE_TYPE]": "TELEGRAM",
+    }
+    logger.info("save lead: %s", params)
     async with httpx.AsyncClient(base_url=SETTINGS.BITRIX_URL) as client:
-        client.post(
-            "",
-            params={
-                "FIELDS[TITLE]": "Новый лид",
-                "FIELDS[NAME]": message.contact.first_name,
-                "FIELDS[LAST_NAME": message.contact.last_name,
-                # Telegram contact doesn't have an email
-                # "FIELDS[EMAIL][0][VALUE]": message.contact.email,
-                # "FIELDS[EMAIL][0][VALUE_TYPE]": "WORK",
-                "FIELDS[PHONE][0][VALUE]": message.contact.phone_number,
-                "FIELDS[PHONE][0][VALUE_TYPE]": "TELEGRAM",
-            },
-        )
+        resp = await client.post("", params=params)
+
+    if resp.status_code != 200:
+        logger.error("save lead: %s", resp.text)
+    else:
+        logger.info("save lead: %s", resp.text)
 
 
 @dispatcher.dp.message_handler(state=Form.lead, content_types=types.ContentType.CONTACT)
@@ -32,4 +36,5 @@ async def process_lead(message: types.Message, state: FSMContext):
     logger.info("%s", message.contact)
     await state.update_data(lead=message.contact.as_json())
     await state.set_state(None)
+    await save_lead(message)
     await message.reply(messages.THANKS, reply_markup=types.ReplyKeyboardRemove())
--- cost_my_chemo_bot/config.py
+++ cost_my_chemo_bot/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     ONCO_MEDCONSULT_API_PASSWORD: SecretStr
 
     BITRIX_URL: HttpUrl = (
-        "https://b24-f64ofq.bitrix24.ru/rest/1/fy0jdv5omjbw21w6/crm.lead.add.jsonK"
+        "https://b24-f64ofq.bitrix24.ru/rest/1/fy0jdv5omjbw21w6/crm.lead.add.json"
     )
 
     TELEGRAM_BOT_TOKEN: str
