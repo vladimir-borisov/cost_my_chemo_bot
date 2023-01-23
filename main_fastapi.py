47f0e360a3f0d974cc517ab13d27cfe8eb184b5a
eremeevfd <f.eremeev@haut.ai>
2023-01-24 01:23:32 +0400

    add security

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,10 +1,12 @@
 import json
+import secrets
 
 import uvicorn
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.dispatcher.filters import Command, Text
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from logfmt_logger import getLogger
 
 from cost_my_chemo_bot.bots.telegram import filters
@@ -26,6 +28,7 @@ from cost_my_chemo_bot.config import SETTINGS, WEBHOOK_SETTINGS
 from cost_my_chemo_bot.db import DB
 
 logger = getLogger(__name__)
+
 
 bot = Bot(token=SETTINGS.TELEGRAM_BOT_TOKEN)
 storage = RedisStorage2(
@@ -96,6 +99,27 @@ async def init_bot():
 
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+async def check_creds(credentials: HTTPBasicCredentials = Depends(security)):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = SETTINGS.ONCO_MEDCONSULT_API_LOGIN.encode("utf8")
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = SETTINGS.ONCO_MEDCONSULT_API_PASSWORD.encode("utf8")
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @app.on_event("startup")
@@ -119,22 +143,22 @@ async def bot_webhook(update: dict):
 
 
 @app.get("/db/courses/")
-async def get_db_courses():
+async def get_db_courses(credentials: HTTPBasicCredentials = Depends(check_creds)):
     return DB().courses
 
 
 @app.get("/db/nosologies/")
-async def get_db_nosologies():
+async def get_db_nosologies(credentials: HTTPBasicCredentials = Depends(check_creds)):
     return DB().courses
 
 
 @app.get("/db/categories/")
-async def get_db_categories():
+async def get_db_categories(credentials: HTTPBasicCredentials = Depends(check_creds)):
     return DB().categories
 
 
 @app.post("/db/reload/")
-async def reload_db():
+async def reload_db(credentials: HTTPBasicCredentials = Depends(check_creds)):
     await DB().reload_db()
     return {"ok": True}
 
